8b082a8caa382d7996b20d6af1cfa000d87d611d
Cristian Suárez Vera <cristian.suarez.ulpgc@gmail.com>
2022-06-17 19:08:33 +0100

    feat: get information from toggl api

--- TogglTrackRepository.py
+++ TogglTrackRepository.py
@@ -1,16 +1,40 @@
 # go to https://track.toggl.com/profile on the section "API token" you can get yours
-# Get workspace id
-# curl -v -u API_TOKEN:api_token -X GET https://api.track.toggl.com/api/v8/workspaces
-# other way to get workspace id is going into reports section on the web. In the URL
-# after the summary you will see the ID
-# https://track.toggl.com/reports/summary/WORKSPACE_ID/period/thisMonth
 
+import os
 
-# Common and basic parameters for request
-# https://github.com/toggl/toggl_api_docs/blob/master/reports.md#request-parameters
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+from io import StringIO
 
-# Detailed report docs
-# https://github.com/toggl/toggl_api_docs/blob/master/reports/detailed.md
+load_dotenv()
 
-# sample request
-# curl -v -u MY_API_TOKEN:api_token -X GET "https://api.track.toggl.com/reports/api/v2/details?workspace_id=WOKSPACE_ID&since=2022-05-01&until=2022-06-01&user_agent=mail@some.domain"
+TOGGL_PROFILE_TOKEN = os.getenv("TOGGL_PROFILE_TOKEN")
+
+workspaces = requests.get(
+    'https://api.track.toggl.com/api/v8/workspaces',
+    auth=(TOGGL_PROFILE_TOKEN, "api_token"),
+)
+
+# from now, we will take just the first workspace_id
+first_workspace_id = workspaces.json()[0]["id"]
+
+details = requests.get(
+    "https://api.track.toggl.com/reports/api/v2/details/.csv",
+    auth=(TOGGL_PROFILE_TOKEN, "api_token"),
+    params={
+        "workspace_id": first_workspace_id,
+        "since": "2022-05-01",
+        "until": "2022-06-01",
+        "user_agent": "mail@some.domain"
+    }
+)
+
+# we delete the first 3 characters because are weird ï»¿
+# in some point we should investigate why is coming
+# if it does not come we are deleting 'Use' from 'User' header
+detailed_report = details.text[3:]
+
+df = pd.read_csv(StringIO(detailed_report))
+
+print(df.head())
